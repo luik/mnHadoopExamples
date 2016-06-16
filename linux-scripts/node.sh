6b9c9e1f3fb2273cb62d5a1e78ce05b45751c9e8
Luis Yucra <luis.yucra@gmail.com>
2016-06-16 16:09:33 -0500

    update node script

--- linux-scripts/node.sh
+++ linux-scripts/node.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+node_address="node address wasn't set";
+gateway_address="gateway address wasn't set";
+broadcast_address="broadcast address wasn't set"
+
+while getopts ":a:" opt; do
+	case $opt in
+		a)
+			ip_address=$OPTART;
+		;;
+		g)
+			gateway_address=$OPTART;
+		;;
+		b)
+			broadcast_address=$OPTART;
+		;;
+	esac
+done
+
 cd ~ 
 sudo apt-get install vim git openjdk-8-jdk-headless ssh
 git clone https://github.com/luik/mnHadoopExamples
@@ -8,3 +26,22 @@ chmod 700 .ssh
 cp mnHadoopExamples/keys/key.pub ~/.ssh/authorized_keys
 chmod 600 ~/.ssh/authorized_keys
 sudo service sshd restart
+
+echo "
+--- a/etc/network/interfaces
++++ b/etc/network/interfaces
+@@ -9,4 +9,10 @@ iface lo inet loopback
+
+ # The primary network interface
+ auto ens33
+-iface ens33 inet dhcp
++iface ens33 inet static
++ address $node_address
++ netmask 255.255.255.0
++ broadcast $broadcast_address
++ gateway $gateway_address
++dns-nameservers 192.168.202.2
++
+" | sudo patch -u -N /etc/network/interfaces -
+
+sudo service network restart
