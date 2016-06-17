c1da5067a74ada6dd00f738b53ce9a36ca798711
Luis Yucra <luis.yucra@gmail.com>
2016-06-16 19:36:12 -0500

    flush ip add

--- linux-scripts/node.sh
+++ linux-scripts/node.sh
@@ -4,10 +4,10 @@ node_address="node address wasn't set";
 gateway_address="gateway address wasn't set";
 broadcast_address="broadcast address wasn't set"
 
-while getopts ":a:g:b" opt; do
+while getopts "a:g:b:" opt; do
 	case $opt in
 		a)
-			ip_address=$OPTARG;
+			node_address=$OPTARG;
 		;;
 		g)
 			gateway_address=$OPTARG;
@@ -29,18 +29,20 @@ sudo service sshd restart
 echo "
 --- a/etc/network/interfaces
 +++ b/etc/network/interfaces
-@@ -9,4 +9,10 @@ iface lo inet loopback
-
+@@ -9,4 +9,9 @@
+ 
  # The primary network interface
  auto ens33
 -iface ens33 inet dhcp
 +iface ens33 inet static
-+ address $node_address
-+ netmask 255.255.255.0
-+ broadcast $broadcast_address
-+ gateway $gateway_address
++address $node_address
++netmask 255.255.255.0
++gateway $gateway_address
 +dns-nameservers $gateway_address
 +
 " | sudo patch -u -N /etc/network/interfaces -
 
-sudo service networking restart
+sudo service networking stop
+sudo ip addr flush dev ens33
+sudo service networking start
+
